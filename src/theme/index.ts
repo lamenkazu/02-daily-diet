b420226544b2aa0200207e61449757c7458e5bca
Lamenkazu <333.erick@gmail.com>
2024-06-28 10:20:09 -0300

    ⚙️ Styles Config Dependencies

---
+++ src/theme/index.ts
@@ -0,0 +1,33 @@
+export default {
+  colors: {
+    white: "#FFFFFF",
+
+    green_dark: "#639339",
+    green_mid: "#CBE4B4",
+    green_light: "#E5F0DB",
+
+    red_dark: "#BF3B44",
+    red_mid: "#F3BABD",
+    red_light: "#F4E6E7",
+
+    gray_700: "#FAFAFA",
+    gray_600: "#EFF0F0",
+    gray_500: "#DDDEDF",
+    gray_400: "#B9BBBC",
+    gray_300: "#5C6265",
+    gray_200: "#333638",
+    gray_100: "#1B1D1E",
+  },
+  fontFamily: {
+    regular: "NunitoSans_400Regular",
+    bold: "NunitoSans_700Bold",
+  },
+  fontSize: {
+    XS: '12px',
+    SM: "14px",
+    MD: "16px",
+    LG: "18px",
+    XL: "24px",
+    XXL: "32px",
+  },
+};
